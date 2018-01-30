d45deea2edc75f28592f26f6d99bf063a42f89ea
Lan Phan <lanc_phan@hotmail.com>
2018-01-30 16:39:56 -0500

    add an event onclick when you visit a store

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,12 +3,27 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+
+  // This is like React's addEventListener
+  goToStore(event){
+    event.preventDefault();
+    console.log("you changed the URL");
+
+    // first grab the text from the box
+    const storeId = this.storeName.value;
+    console.log(`Going to ${storeId}`);
+
+    // second, we're going to transition rom / to /store/:storeId
+    this.context.router.transitionTo(`/store/${storeId}`);
+    // To get 'transitionTo', we need contextTypes and PropTypes from line 32
+  }
+
   render() {
     // Any where else
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(name) => { this.storeName = name}}/>
         <button type="submit">Visit Store →</button>
       </form>
     )
