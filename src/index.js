837ebfe3ea84fc7366a852432b79b91aae678c0b
Lan Phan <lanc_phan@hotmail.com>
2018-01-30 16:33:05 -0500

    add routing with React Router 4. Similar to making resources in the route.rb of Ruby on rails

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
 
 // STYLING
 import './css/style.css';
@@ -7,5 +8,21 @@ import './css/style.css';
 // COMPONENTS
 import App from './components/App';
 import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
 
-render(<App/>, document.querySelector('#main'));
+// STATELESS FUNCTIONS WITH ROUTES
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={StorePicker}/>
+        <Match pattern="/store/:storeId" component={App}/>
+        <Miss component={NotFound}/>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+// With BrowserRouter, one can go to the all three routes BUT manually. We'll need to use context route in StorePicker to automatically go to the store/:storeId.
+
+render(<Root/>, document.querySelector('#main'));
