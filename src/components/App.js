884ddb35d1157145437ef506b9c0784c18835fd8
Lan Phan <lanc_phan@hotmail.com>
2018-01-30 18:16:15 -0500

    Make a addFish function on the App component that can 'swim' down to the Inventory component to the AddFishForm component. Also, bind it to the constructor

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,30 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.addFish = this.addFish.bind(this);
+
+    // get initial state
+    this.state = {
+      fishes: {},
+      order: {}
+    };
+  }
+
+  // This methods will let the fish 'swim' up to the App.js from AddFishForm.js
+  addFish(fish){
+
+    // udpate our state
+    const fishes = {...this.state.fishes}; // takes the current fishes state and put it into a new fishes state.
+
+    // add in our new fish
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+    // set state
+    this.setState({ fishes }); // alternative: fishes: fishes (line 13: line 22)
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -12,7 +36,9 @@ class App extends React.Component {
           <Header tagline="You can’t it eat just one"/>
         </div>
         <Order />
-        <Inventory/>
+        <Inventory
+          addFish={this.addFish}
+          />
       </div>
     )
   }
