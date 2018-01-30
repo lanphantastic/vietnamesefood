0f21cde2c9174cf27a4237f64144616ef9475081
Lan Phan <lanc_phan@hotmail.com>
2018-01-30 16:26:50 -0500

    add the header with a svg of pho

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,18 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-      <p>Header</p>
+      <header className="top">
+        <h1>Vietnamese</h1>
+        <h1>
+          Food
+          <span className="ofThe">
+            <span className="of">of</span>
+            <span className="the">the</span>
+          </span>
+          Day
+        </h1>
+      <h3 className="tagline">{this.props.tagline}</h3>
+      </header>
     )
   }
 }
