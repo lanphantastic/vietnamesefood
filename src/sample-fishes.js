5de300c944454e5085c93f8682539ccb267baa23
Lan Phan <lanc_phan@hotmail.com>
2018-01-30 21:48:30 -0500

    add vietnamese dishes name, description, price, and photo

--- src/sample-fishes.js
+++ src/sample-fishes.js
@@ -1,74 +1,74 @@
 // This is just some sample data so you don't have to think of your own!
 const fishes = {
   fish1: {
-    name: 'Pacific Halibut',
-    image: 'https://media.istockphoto.com/photos/grilled-halibut-with-spinach-leeks-and-pine-nuts-picture-id503337620?k=6&m=503337620&s=612x612&w=0&h=C2RuTj4IDKvEvkC0J2i_7x2rnMlspq8CTcVVtdqd5u8=',
-    desc: 'Everyones favorite white fish. We will cut it to the size you need and ship it.',
-    price: 1724,
+    name: 'Bánh Mì',
+    image: 'https://www.epicurious.com/images/articlesguides/diningtravel/restaurants/51252290_breakfast-banh-mi_612.jpg',
+    desc: 'Everyones favorite grab and go. We will cut it to the size you need and ship it.',
+    price: 300,
     status: 'available'
   },
 
   fish2: {
-    name: 'Lobster',
-    image: 'https://i.istockimg.com/file_thumbview_approve/32135274/5/stock-photo-32135274-cooked-lobster.jpg',
-    desc: 'These tender, mouth-watering beauties are a fantastic hit at any dinner party.',
-    price: 3200,
+    name: 'Bún bò Huế',
+    image: 'https://kingsfoodmarkets.com/uploads/recipes-multi-size/Bun_bo_Hue%2C_Vietnamese_soup_108755138.jpg',
+    desc: 'Noodle soup containing rice vermicelli and beef and greatly admired for its balance of spicy, sour, salty and sweet flavors and lemon grass.',
+    price: 1100,
     status: 'available'
   },
 
   fish3: {
-    name: 'Sea Scallops',
-    image: 'https://media.istockphoto.com/photos/seared-scallop-picture-id533292475?k=6&m=533292475&s=612x612&w=0&h=kqpWf3BxavUmDyT8O-c3UK-TD5qC5Y1_j4IeFl5mXf0=',
-    desc: 'Big, sweet and tender. True dry-pack scallops from the icey waters of Alaska. About 8-10 per pound',
-    price: 1684,
+    name: 'Banh Bo Hap',
+    image: 'https://i.ytimg.com/vi/xzOOlXw3Y4I/maxresdefault.jpg',
+    desc: 'sweet, soft and deliciously spongy cake made from rice flour, water, sugar, and yeast.',
+    price: 500,
     status: 'unavailable'
   },
 
   fish4: {
-    name: 'Mahi Mahi',
-    image: 'https://i.istockimg.com/file_thumbview_approve/12556651/5/stock-photo-12556651-mahimahi.jpg',
-    desc: 'Lean flesh with a mild, sweet flavor profile, moderately firm texture and large, moist flakes. ',
-    price: 1129,
+    name: 'Bánh xèo',
+    image: 'http://ukcdn.ar-cdn.com/recipes/port500/7267d65d-1021-4b80-99c6-133390be9952.jpg',
+    desc: 'Named for the loud sizzling sound it makes when the rice batter is poured into the hot skillet is a Vietnamese savory fried pancake made of rice flour, water, turmeric powder.',
+    price: 700,
     status: 'available'
   },
 
   fish5: {
-    name: 'King Crab',
-    image: 'https://media.istockphoto.com/photos/cooked-organic-alaskan-king-crab-legs-picture-id501882496?k=6&m=501882496&s=612x612&w=0&h=eoazPxDMjdb37qPEq6xpwCwwM6f__35S7lNglIflryY=',
-    desc: 'Crack these open and enjoy them plain or with one of our cocktail sauces',
-    price: 4234,
+    name: 'Pho Dac Biet',
+    image: 'https://steamykitchen.com/wp-content/uploads/2008/02/vietnamese-pho-recipe-2.jpg',
+    desc: 'Consisting of broth, rice noodles, a few herbs, and meat, primarily made with either beef (phở bò) or chicken (phở gà).',
+    price: 950,
     status: 'available'
   },
 
   fish6: {
-    name: 'Atlantic Salmon',
-    image: 'https://i.istockimg.com/file_thumbview_approve/56241842/5/stock-photo-56241842-salmon-fish.jpg',
-    desc: 'This flaky, oily salmon is truly the king of the sea. Bake it, grill it, broil it...as good as it gets!',
-    price: 1453,
+    name: 'Com Tam',
+    image: 'http://aromasian.com/wp-content/uploads/2017/08/Vietnamese-Broken-Rice-Com-Tam-1.jpg',
+    desc: 'Served with grilled pork (either ribs or shredded) plus the Vietnamese dish bì (thinly shredded pork mixed with cooked and thinly shredded pork skin) over broken rice',
+    price: 1000,
     status: 'available'
   },
 
   fish7: {
-    name: 'Oysters',
-    image: 'https://i.istockimg.com/file_thumbview_approve/58626682/5/stock-photo-58626682-fresh-oysters-on-a-black-stone-plate-top-view.jpg',
-    desc: 'A soft plump oyster with a sweet salty flavor and a clean finish.',
-    price: 2543,
+    name: 'Goi cuon',
+    image: 'http://d1v827hezncazj.cloudfront.net/wp-content/uploads/2009/10/goi-cuon-recipe.jpg',
+    desc: 'Consisting of pork, prawn, vegetables, rice vermicelli, and other ingredients wrapped in Vietnamese bánh tráng (commonly known as rice paper).',
+    price: 650,
     status: 'available'
   },
 
   fish8: {
-    name: 'Mussels',
-    image: 'https://i.istockimg.com/file_thumbview_approve/40450406/5/stock-photo-40450406-steamed-mussels.jpg',
-    desc: 'The best mussels from the Pacific Northwest with a full-flavored and complex taste.',
+    name: 'Banh cuon',
+    image: 'https://media.foody.vn/res/g18/178106/prof/s640x400/foody-mobile-t1-jpg-559-635809453498630683.jpg',
+    desc: 'A thin, wide sheet of steamed fermented[2] rice batter filled with a mixture of cooked seasoned ground pork, minced wood ear mushroom, and minced shallots.',
     price: 425,
     status: 'available'
   },
 
   fish9: {
-    name: 'Jumbo Prawns',
+    name: 'Banh Tet',
     image: 'https://i.istockimg.com/file_thumbview_approve/67121439/5/stock-photo-67121439-fresh-tiger-shrimp-on-ice-on-a-black-stone-table.jpg',
-    desc: 'With 21-25 two bite prawns in each pound, these sweet morsels are perfect for shish-kabobs.',
-    price: 2250,
+    desc: 'A savoury but sometimes sweetened cake made primarily from glutinous rice, rolled in a banana leaf with a mung bean or mung bean and pork filling.',
+    price: 1500,
     status: 'available'
   }
 };
