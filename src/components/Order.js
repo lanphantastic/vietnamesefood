8fef0e73223c8bd6d2241a4d167d9d2be6e8dce6
Lan Phan <lanc_phan@hotmail.com>
2018-01-30 22:15:09 -0500

    pass the states, fishes and order, into the Order component to be access. Create a total calculate whenever you add food to order

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { formatPrice } from '../helpers';
 
 class Order extends React.Component {
   render() {
+    // access the value of fish1, fish2, fish3, etc. as an array
+    const orderIds = Object.keys(this.props.order);
+
+    const total = orderIds.reduce((prevTotal, key) => {
+
+      // access the particular fish and its count from the fishes and order objects. fish = fish1: and all of its keys/values.
+
+      // remember that fishes is a hash with many fish1: desc, image..., fish2: desc, image, price, etc. The same go with order object; order: {{fish1: #}, {fish2: #}}
+      const fish = this.props.fishes[key]; // access the key/pair of that fish we further use its name, price, desc etc.
+      const count = this.props.order[key]; //access the value of that fish.
+
+      // check if the seafood is available to add to order
+      const isAvailable = fish && fish['status'] === 'available';
+      if(isAvailable) {
+        return prevTotal + (count * fish['price'] || 0)
+      }
+      return prevTotal;
+    }, 0);
+
     return (
-      <p>Order</p>
+      <div className="oreder-wrap">
+        <h2>Your Order</h2>
+        <ul className="order">
+          <p>{orderIds}</p>
+          <li className="total">
+            <strong>Total:</strong>{formatPrice(total)}
+          </li>
+        </ul>
+      </div>
     )
   }
 }
